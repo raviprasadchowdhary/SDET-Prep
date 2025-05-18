ddceb24778cd5ba6c9759ac7eb82825db37eca59
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-05-18 15:34:30 +0530

    dummy

---
+++ src/main/java/com/practice/corejava/basics/PrimeNumber.java
@@ -0,0 +1,12 @@
+package com.practice.corejava.basics;
+
+public class PrimeNumber {
+	public static void main(String[] args){
+		int input = 25;
+
+		boolean isPrime = true;
+		for (int i = 0; i <= input/2; i++){
+
+		}
+	}
+}
--- src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-	public static void main(String[] args) {
-		//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-		// to see how IntelliJ IDEA suggests fixing it.
-		System.out.println("Hello and welcome!");
-	}
-}

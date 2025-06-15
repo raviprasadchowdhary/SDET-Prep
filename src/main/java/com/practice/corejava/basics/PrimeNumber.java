8917a952f65af52f3506023bf22188e2bf48ec07
Raviprasad <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-06-15 20:39:48 +0530

    prime number code

--- src/main/java/com/practice/corejava/basics/PrimeNumber.java
+++ src/main/java/com/practice/corejava/basics/PrimeNumber.java
@@ -1,12 +1,31 @@
 package com.practice.corejava.basics;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
-	public static void main(String[] args){
-		int input = 25;
+    public static void main(String[] args) {
 
-		boolean isPrime = true;
-		for (int i = 0; i <= input/2; i++){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int input = scanner.nextInt();
+        scanner.close();
 
-		}
-	}
+        boolean isPrime = true;
+        if (input <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(input); i++) {
+                if (input % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(input + " is a prime number");
+        } else {
+            System.out.println(input + " isn't a prime number (Prime numbers are greater than 1)");
+        }
+    }
 }
